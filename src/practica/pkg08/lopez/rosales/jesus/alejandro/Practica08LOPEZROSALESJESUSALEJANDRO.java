17acfe37d0171dea16c0d42a57c412aebad2f5f4
JesusLopez28 <jesus.lopez280402@gmail.com>
2023-04-26 22:48:19 -0500

    Cambios finales

--- src/practica/pkg08/lopez/rosales/jesus/alejandro/Practica08LOPEZROSALESJESUSALEJANDRO.java
+++ src/practica/pkg08/lopez/rosales/jesus/alejandro/Practica08LOPEZROSALESJESUSALEJANDRO.java
@@ -1,22 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ López Rosales Jesús Alejandro
+ 22110104       3°O        POE
  */
-
 package practica.pkg08.lopez.rosales.jesus.alejandro;
 
-/**
- *
- * @author PC
- */
 public class Practica08LOPEZROSALESJESUSALEJANDRO {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        FormMenu menu = new FormMenu();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
     }
     
 }
