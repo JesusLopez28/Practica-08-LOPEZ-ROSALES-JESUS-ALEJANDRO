15580e0c9d1a23c62169afc89dcccd6e7f3d55a0
JesusLopez28 <jesus.lopez280402@gmail.com>
2023-04-26 22:44:42 -0500

    Cambios finales

---
+++ src/practica/pkg08/lopez/rosales/jesus/alejandro/Practica08LOPEZROSALESJESUSALEJANDRO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practica.pkg08.lopez.rosales.jesus.alejandro;
+
+/**
+ *
+ * @author PC
+ */
+public class Practica08LOPEZROSALESJESUSALEJANDRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
